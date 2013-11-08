65c3bda3e3ed1d77b669784d251db308ce446fbc
David G <david.guyueyi@gmail.com>
2013-11-08 12:41:33 +0800

    Andciv V3: add JLink flash download script

--- nuttx/configs/andciv-v3/scripts/flash_download.sh
+++ nuttx/configs/andciv-v3/scripts/flash_download.sh
@@ -1,11 +1,10 @@
 #! /bin/sh
 
 JLinkExe << EOF
-exec EnableFlashDL
-exec Device=LPC1758
+exec device = lpc1758
 speed auto
 halt
-loadbin nuttx.bin 0x00001000
+loadbin nuttx.bin,0x00001000
 reset
 go
 exit
